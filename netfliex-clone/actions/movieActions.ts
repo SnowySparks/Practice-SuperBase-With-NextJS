1d9e87698dcef12b6a0c91e253f69d51d56e612b
SnowyPacks <originpc@naver.com>
2024-12-10 17:33:41 +0900

    SEO 처리

--- netfliex-clone/actions/movieActions.ts
+++ netfliex-clone/actions/movieActions.ts
@@ -20,3 +20,18 @@ export const searchMovies = async (search = "") => {
   }
   return data;
 };
+
+export const getMovie = async (id: number) => {
+  const supabase = await createServerSupabaseClient();
+
+  const { data, error } = await supabase
+    .from("movie")
+    .select("*")
+    .eq("id", id)
+    .maybeSingle(); //-> 리스트러 이 데이터를 받지 않고, 하나인 것은 무조건 알고 있음 그러나 Null 일수도 있음
+
+  if (error) {
+    handleError(error);
+  }
+  return data;
+};
