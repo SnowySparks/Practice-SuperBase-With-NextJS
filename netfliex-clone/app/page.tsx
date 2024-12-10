f671f2e01cd8b49a52b555be8e37a48f1b4e515d
SnowyPacks <originpc@naver.com>
2024-12-11 00:34:55 +0900

    SEO 추가 업데이트

--- netfliex-clone/app/page.tsx
+++ netfliex-clone/app/page.tsx
@@ -3,7 +3,12 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "TMDBFLIX",
-  description: "Netflix clone using TMDB API",
+  description: "영화 목록 사이트 (연습)",
+  openGraph: {
+    title: "TMDBFLIX",
+    description: "영화 목록 사이트 (연습)",
+    images: ["/images/tmdbflix_logo.png"],
+  },
 };
 
 export default function Home() {
