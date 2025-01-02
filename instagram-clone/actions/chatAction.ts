2c76fa6ddde33665fa3ef27a4a62df1ab2e7292b
SnowyPacks <originpc@naver.com>
2025-01-02 15:33:53 +0900

    메세지 전송 및 수신 서버 액션 구현

--- instagram-clone/actions/chatAction.ts
+++ instagram-clone/actions/chatAction.ts
@@ -1,6 +1,9 @@
 "use server";
 
-import { createServerSupabaseAdminClient } from "utils/supabase/server";
+import {
+  createServerSupabaseAdminClient,
+  createServerSupabaseClient,
+} from "utils/supabase/server";
 
 export async function getAllUsers() {
   // 모든 유저의 데이터를 가져오기 위해 Admin 접근
@@ -21,4 +24,57 @@ export async function getUserById(userId: string) {
 
   if (error) return null;
   return data.user;
+}
+
+// 메세지 전송
+export async function sendMessage({
+  message,
+  chatUserId,
+}: {
+  message: string;
+  chatUserId: string;
+}) {
+  const supabase = await createServerSupabaseClient();
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+
+  if (error || !session.user) {
+    throw new Error("User is not authenticated");
+  }
+  const { data, error: SendMessageError } = await supabase
+    .from("message")
+    .insert({
+      message,
+      receiver: chatUserId,
+      sender: session.user.id,
+      is_deleted: false,
+    });
+
+  if (SendMessageError) {
+    throw new Error(SendMessageError.message);
+  }
+  return data;
+}
+
+// 메세지 전부 받기
+export async function getAllMessages({ chatUserId }: { chatUserId: string }) {
+  const supabase = await createServerSupabaseClient();
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+
+  if (error || !session.user) {
+    throw new Error("User is not authenticated");
+  }
+  const { data, error: getAllMessagesError } = await supabase
+    .from("message")
+    .select("*")
+    .or(`receiver.eq.${session.user.id},receiver.eq.${chatUserId}`)
+    .or(`sender.eq.${session.user.id},sender.eq.${chatUserId}`)
+    .order("created_at", { ascending: true });
+  if (getAllMessagesError) return [];
+  return data;
 }
